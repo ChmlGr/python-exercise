0607883debb3e966ef68bb7bfd8970d261f5bc2b
Ximena Garcia <xgarcia32@gmail.com>
2019-09-21 16:08:03 -0300

    Part 1 - Requirement 4

--- part1/src/python_exercise/__main__.py
+++ part1/src/python_exercise/__main__.py
@@ -1,4 +1,5 @@
 import click
+from cfn_flip import to_json
 from commons import commons
 
 
@@ -17,3 +18,13 @@ def check_name(project_name):
     click.echo(commons.is_palindrome(project_name))
 
 
+@main.command()
+@click.argument('yaml_file')
+def yaml_to_json(yaml_file):
+    with open(yaml_file, 'r') as file, \
+            open('tacocat_cf.json', 'w', encoding='utf-8') as output:
+        try:
+            output.write(to_json(file.read()))
+            click.echo('File written successfully')
+        except Exception as e:
+            click.echo(e)
