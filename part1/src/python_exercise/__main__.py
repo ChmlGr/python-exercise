e4929be27ddb9c62b0ae837ca9b432eca2133be1
Ximena Garcia <xgarcia32@gmail.com>
2019-09-21 15:14:45 -0300

    Part 1 - Requirement 3

--- part1/src/python_exercise/__main__.py
+++ part1/src/python_exercise/__main__.py
@@ -1,4 +1,5 @@
 import click
+from commons import commons
 
 
 @click.group()
@@ -7,3 +8,12 @@ def main():
     Thank you for asking for help. These tools are used to generate a Django project and convert CloudFormation code from YAML to JSON
     """
     pass
+
+
+@main.command()
+@click.argument('project_name')
+def check_name(project_name):
+    click.echo('Is this project name a palindrome ? ', nl=False)
+    click.echo(commons.is_palindrome(project_name))
+
+
